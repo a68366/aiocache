fe117ae2b2477837067eb07d45430e05d6c3a409
a68366 <a68366@users.noreply.github.com>
2023-01-02 12:56:20 +0300

    tests: make Keys enum behave the same with python 3.11

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,10 @@
-class Keys:
+from enum import Enum
+
+
+class Keys(str, Enum):
     KEY: str = "key"
     KEY_1: str = "random"
     KEY_LOCK: str = "key-lock"
+    
+    def __format__(self, spec):
+        return self.value
